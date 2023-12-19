1b4cccd2ae2ccc4b8e8e45754247cb79348eba5a
Andriy Babiy <andriybabiy97@gmail.com>
2023-12-19 08:35:21 +0100

    Changes to HTML for Github Pages

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,5 @@
 /**
- * Write a script that takes in a number via na input field,
+ * Write a function that takes in a number via na input field,
  * upon clicking the 'create' button it will create the oulined number of elements,
  * each of the elements is assigned a random number and sequentially increases in width and height by 10px.
  * There is also another button labelled 'destroy',
@@ -20,25 +20,25 @@ const destroy = controls.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
 // adding event listeners for when the buttons are clicked
-create.addEventListener('click', handlerCreate);
-destroy.addEventListener('click', handlerDestroy);
+create.addEventListener('click', () => createBoxes(amount.value));
+destroy.addEventListener('click', () => destroyBoxes());
 
 // this is the create event that creates the outlined number of boxes and places them into the box div#boxes.
-function handlerCreate() {
+function createBoxes(int) {
   // create the markup of the box elements that sequentially increase in sise
   const markup = (int) => `
-    <div style="height:${30 + (10 * int)}px; width:${30 + (10 * int)}px; background-color: ${getRandomHexColor()}">
-    </div> `;
+  <div style="height:${30 + (10 * int)}px; width:${30 + (10 * int)}px; background-color: ${getRandomHexColor()}">
+  </div> `;
   
   // iterate the creation of boxes for the amount of boxes that the user outlines
-  for (let i = 0; i < amount.value; i++) {
+  for (let i = 0; i < int; i++) {
     // boxes are added sequentially with the smallest box on top
     boxes.insertAdjacentHTML('beforeend', markup(i));
   }
 }
 
 // this is the handler function that occuers when the destroy button is clicked.
-function handlerDestroy() {
+function destroyBoxes() {
   // the elements inside the div div#boxes are removed and returned to the original empty state.
   boxes.innerHTML = ``;
 }
