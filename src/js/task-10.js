131f0713b1723fdbbeaac77bef71ab074dd37bc9
Andriy Babiy <andriybabiy97@gmail.com>
2023-12-21 13:31:58 +0100

    Induced the recommended changes to q10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -12,33 +12,30 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// obtaining access to the HTML elements via the DOM
 const controls = document.querySelector('#controls');
 const amount = controls.querySelector('input[type="number"]');
 const create = controls.querySelector('button[data-create]');
 const destroy = controls.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
-// adding event listeners for when the buttons are clicked
 create.addEventListener('click', () => createBoxes(amount.value));
 destroy.addEventListener('click', () => destroyBoxes());
 
-// this is the function that creates the outlined number of boxes and places them into the box div#boxes.
 function createBoxes(int) {
-  // create the markup of the box elements that sequentially increase in sise
+  destroyBoxes();
+  
   const markup = (int) => `
   <div style="height:${30 + (10 * int)}px; width:${30 + (10 * int)}px; background-color: ${getRandomHexColor()}">
   </div> `;
   
-  // iterate the creation of boxes for the amount of boxes that the user outlines.
+  const elements = [];
   for (let i = 0; i < int; i++) {
-    // boxes are added sequentially with the smallest box on top
-    boxes.insertAdjacentHTML('beforeend', markup(i));
+    elements.push(markup(i));
   }
+  
+  boxes.insertAdjacentHTML('beforeend', elements.join(''));
 }
 
-// this is the function that occurs when the destroy button is clicked.
 function destroyBoxes() {
-  // the elements inside the div div#boxes are removed and returned to the original empty state.
-  boxes.innerHTML = ``;
+    boxes.innerHTML = ``;
 }
